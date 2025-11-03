3dc3af4def7d43d3724d7431088f5fbed6bb7679
lepmoranrp <phillip.moran@rocketpartners.io>
2025-11-04 01:03:55 +0800

    feat(added-shared-types)

--- apps/rp-hris-api/src/app/app.service.ts
+++ apps/rp-hris-api/src/app/app.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@rp-hris/shared';
 
 @Injectable()
 export class AppService {
 
-  getData(): { message: string} {
-
-    return ({ message: 'Hello API' });
+  getData(): { message: string, user: User } {
+    // Sample Usage of the User Context
+    const user: User = {
+      id: "1",
+      name: "John Doe",
+      email:"john@gmail.com"
+    }
+    return ({ message: 'Hello API', user: user });
   }
 }
